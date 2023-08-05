d16b211e05cba06f34bfd6fe3663b18ad0f353b1
howl201 <104803826+howl201@users.noreply.github.com>
2023-08-05 19:39:05 +0900

    Update start-manager.sh (Fix config file name)

--- start-manager.sh
+++ start-manager.sh
@@ -3,5 +3,5 @@
 docker container prune
 sudo rm -Ir workdir
 mkdir workdir
-sudo taskset -c 9-11 bin/syz-manager -vv 100 -config config/gvisor.json 2>&1 | tee manager.log
+sudo taskset -c 9-11 bin/syz-manager -vv 100 -config config/docker.json 2>&1 | tee manager.log
 # ADDING -debug locks the number of fuzzing procs to 1 always
